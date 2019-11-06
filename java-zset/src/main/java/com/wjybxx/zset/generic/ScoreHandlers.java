5aa4db43d0fed7d1d70094fbc70f7970a5e5daf9
wjybxx <845740757@qq.com>
2019-11-06 17:06:42 +0800

    添加逆序的LongScore处理器

--- java-zset/src/main/java/com/wjybxx/zset/generic/ScoreHandlers.java
+++ java-zset/src/main/java/com/wjybxx/zset/generic/ScoreHandlers.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2019 wjybxx
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to iBn writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.wjybxx.zset.generic;
 
 /**
@@ -17,11 +33,19 @@ public class ScoreHandlers {
      * @return Long类型的score处理器
      */
     public static ScoreHandler<Long> longScoreHandler() {
-        return LongScoreHandler.INSTANCE;
+        return longScoreHandler(false);
     }
 
     /**
-     * Long类型的score处理器
+     * @param reverse 是否降序
+     * @return Long类型的score处理器
+     */
+    public static ScoreHandler<Long> longScoreHandler(boolean reverse) {
+        return reverse ? DesLongScoreHandler.INSTANCE : LongScoreHandler.INSTANCE;
+    }
+
+    /**
+     * Long类型的score处理器 - 升序
      */
     private static class LongScoreHandler implements ScoreHandler<Long> {
 
@@ -34,6 +58,29 @@ public class ScoreHandlers {
         @Override
         public int compare(Long o1, Long o2) {
             return o1.compareTo(o2);
+        }
+
+        @Override
+        public Long sum(Long oldScore, Long increment) {
+            return oldScore + increment;
+        }
+    }
+
+    /**
+     * Long类型的score处理器 - 降序
+     */
+    private static class DesLongScoreHandler implements ScoreHandler<Long> {
+
+        private static final DesLongScoreHandler INSTANCE = new DesLongScoreHandler();
+
+        private DesLongScoreHandler() {
+
+        }
+
+        @Override
+        public int compare(Long o1, Long o2) {
+            // 逆序
+            return o2.compareTo(o1);
         }
 
         @Override
